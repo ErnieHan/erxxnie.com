2f1f11ca601d860117b75dd6dce893bda853e06a
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2023-03-28 09:25:46 +0800

    updates

---
+++ src/pages/Blog/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export default function Blog() {
+    const { id } = useParams()
+    return <div>Blog: {id}</div>
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import image from '../../images/01.jpg'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Homepage erxxnie.com</h1>
+            <img src={image} alt="" />
+        </div>
+    )
+}
